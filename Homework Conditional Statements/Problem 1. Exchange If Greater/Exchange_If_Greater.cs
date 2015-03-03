8d11f5c59025fdcb36ab68c70e9ddff6d5bc408f
doktoraaaa <doktoraaaa@gmail.com>
2015-03-03 17:49:06 +0200

    Homework Conditional Statements

---
+++ Homework Conditional Statements/Problem 1. Exchange If Greater/Exchange_If_Greater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Exchange_If_Greater
+{
+    static void Main()
+    {
+        double firstNum = double.Parse(Console.ReadLine());
+        double secNum = double.Parse(Console.ReadLine());
+
+
+        if (firstNum < secNum)
+        {
+            Console.WriteLine("{0} {1}", firstNum, secNum);
+        }
+        else
+        {
+            Console.WriteLine("{0} {1}", secNum, firstNum);
+        }
+    }
+}
