43a088f89960c23833b49dc2544bb179156c1151
doktoraaaa <doktoraaaa@gmail.com>
2015-03-03 17:52:08 +0200

    Homework Conditional Statements

---
+++ Homework Conditional Statements/Problem 2. Bonus Score/Bonus_Score.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Bonus_Score
+{
+    static void Main()
+    {
+        int score = int.Parse(Console.ReadLine());
+
+        switch (score)
+        {
+            case 1:
+            case 2:
+            case 3:
+                Console.WriteLine(score * 10);
+                return;
+            case 4:
+            case 5:
+            case 6:
+                Console.WriteLine(score * 100);
+                return;
+            case 7:
+            case 8:
+            case 9:
+                Console.WriteLine(score * 1000);
+                return;
+            default:
+                Console.WriteLine("invalid score");
+                return;
+        }
+    }
+}
